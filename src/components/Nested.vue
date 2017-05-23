0b707ed66983d0b4f1fe41eb0d0988fa5a4cbabe
Philippe Schraepen <mail@phill.be>
2017-05-23 13:06:04 +0200

    Enable toggling of the checkboxes

--- src/components/Nested.vue
+++ src/components/Nested.vue
@@ -1,16 +1,22 @@
 <template>
-  <div class="nested" data="book">
-    <div class="nested__title">Title: {{data.title}}</div>
-    <div class="nested__title">ID: {{data.id}}</div>
-    <div class="nested__child">Children:
-      <nested
-      class="nested__child__list"
-      v-for='child in data.children'
-      :data='child'
-      :key='child'>
-    </nested>
+  <div class="nested">
+    <div
+    @click.stop='clickCheckbox'
+    :class="'nested__checkbox--' + getStatus"
+    class="nested__checkbox"></div>
+    <div class="nested__content">
+      <div class="nested__title">Title: {{data.title}}</div>
+      <div class="nested__id">ID: {{data.id}}</div>
+      <div class="nested__child">
+        <nested
+        class="nested__child__list"
+        v-for='child in data.children'
+        :data='child'
+        :key='child'>
+        </nested>
+      </div>
+    </div>
   </div>
-</div>
 </template>
 
 <script>
@@ -18,16 +24,55 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 Vue.use(Vuex)
 
+const statuses = {
+  checked: 'checked',
+  unchecked: 'unchecked'
+}
+
+const store = new Vuex.Store({
+  state: {
+    statuses: []
+  },
+  mutations: {
+    updateStatus (state, component) {
+      let existingComponent = state.statuses.find(status => status.id === component.id)
+      if (existingComponent) {
+        existingComponent.status = component.status
+        return
+      }
+      state.statuses.push(component)
+    }
+  },
+  getters: {
+    getComponentStatus: state => componentId => {
+      let component = state.statuses.find(component => component.id === componentId)
+      if (component && component.status) {
+        return component.status
+      }
+      return statuses.unchecked
+    }
+  }
+})
+
 export default {
   name: 'nested',
   props: ['data'],
-  data () {
-    return {
+  store,
+  methods: {
+    clickCheckbox: function () {
+      if (store.getters.getComponentStatus(this.data.id) === statuses.unchecked) {
+        store.commit('updateStatus', {id: this.data.id, status: statuses.checked})
+      } else {
+        store.commit('updateStatus', {id: this.data.id, status: statuses.unchecked})
+      }
     }
   },
-  mounted () {
-
+  computed: {
+    getStatus () {
+      return store.getters.getComponentStatus(this.data.id)
+    }
   }
+
 }
 </script>
 
@@ -35,9 +80,25 @@ export default {
 .nested {
   background: lightgray;
   padding: 20px;
+
+  &__child__list {
+    padding-left: 20px;
+  }
+
+  &__checkbox {
+    width: 10px;
+    height: 10px;
+    cursor: pointer;
+    &--checked {
+      background: green;
+    }
+    &--indetermined {
+      background: yellow;
+    }
+    &--unchecked {
+      background: red;
+    }
+  }
 }
 
-.nested__child__list {
-  padding-left: 20px;
-}
 </style>
