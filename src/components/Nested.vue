ffff27aad48837e3c22f2bada985d16e0b957fff
Philippe Schraepen <mail@phill.be>
2017-05-23 13:01:10 +0200

    Enable SCSS

--- src/components/Nested.vue
+++ src/components/Nested.vue
@@ -31,8 +31,7 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style lang="scss" scoped>
 .nested {
   background: lightgray;
   padding: 20px;
